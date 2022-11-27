d7510cf8b53de6262ed96c041eca836ba6f32e55
quangdang <quangdang2001@gmail.com>
2022-11-27 09:04:41 +0700

    fix google login

--- src/main/java/com/tlcn/demo/service/filter/UserAuthorizationFilter.java
+++ src/main/java/com/tlcn/demo/service/filter/UserAuthorizationFilter.java
@@ -1,11 +1,12 @@
 package com.tlcn.demo.service.filter;
 
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.access.AuthorizationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -28,35 +29,35 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class UserAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+        if (request.getServletPath().equals("/api/login") || request.getServletPath().equals("/api/register")){
+            filterChain.doFilter(request,response);
+        }
         String authorizationHeader = request.getHeader(AUTHORIZATION);
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-
+        if (authorizationHeader!=null && authorizationHeader.startsWith("Bearer ")){
+            String username;
+            Claim roles;
             try {
                 String token = authorizationHeader.substring("Bearer ".length());
                 Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
                 JWTVerifier verifier = JWT.require(algorithm).build();
                 DecodedJWT decodedJWT = verifier.verify(token);
-                String username = decodedJWT.getSubject();
-                Claim roles = decodedJWT.getClaim("role");
-                Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
-                authorities.add(new SimpleGrantedAuthority(roles.asString()));
-                UsernamePasswordAuthenticationToken authenticationToken =
-                        new UsernamePasswordAuthenticationToken(username, null, authorities);
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 
-                filterChain.doFilter(request, response);
-            } catch (Exception e) {
-                System.out.println("Error loggin in: " + e.getMessage());
-                response.setHeader("error", e.getMessage());
-                response.setStatus(FORBIDDEN.value());
-                Map<String, String> error = new HashMap<>();
-                error.put("error_message", e.getMessage());
-                response.setContentType(APPLICATION_JSON_VALUE);
-                new ObjectMapper().writeValue(response.getOutputStream(), error);
+                username = decodedJWT.getSubject();
+                roles = decodedJWT.getClaim("role");
+            }catch (Exception e){
+                throw new AuthorizationServiceException("Error token");
             }
-        } else {
-            filterChain.doFilter(request, response);
+            Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(roles.asString()));
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(username,null,authorities);
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+//                    Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+            filterChain.doFilter(request,response);
+
+        }else {
+            filterChain.doFilter(request,response);
         }
 
 
